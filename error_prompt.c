9864c99185b0aa783f9f0dd81aa5c245d045012c
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-21 13:23:06 -0700

    cmd_invalid

--- error_prompt.c
+++ error_prompt.c
@@ -65,3 +65,36 @@ void not_found(char *str, int c_n, list_t *env);
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "not found\n", 10);
 }
+
+/**
+ * cmd_invalid - Prints error ("sh: 2: cd: cant cd to xyz")
+ * @env: Enviromental variables linked list
+ * @c_n: Command typed by user
+ * @str: Arguement typed after cmd
+ */
+
+void cmd_invalid(list_t *env, int c_n, char *str)
+{
+	char *shell, *num;
+	int count = 0;
+
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	num = int_to_string(c_n);
+	count = 0;
+	while (num[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, count, num);
+	free(num);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "cd: can't cd to " 16);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, "\n", 1);
+}

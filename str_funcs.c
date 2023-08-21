995e7b9f1d5688a7bddba800bc084de7498893f6
Najat <craazylolla@gmail.com>
2023-08-21 07:04:16 -0700

    strcpy

--- str_funcs.c
+++ str_funcs.c
@@ -38,3 +38,48 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: int that tells num spaces in between
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	/* while neither strings have ended*/
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		/* if element in arr1 in arr2 are diff, return difference*/
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		/* iterate index*/
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination string
+ * @src: the string to copy
+ * Return: copy of original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i <= length; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}

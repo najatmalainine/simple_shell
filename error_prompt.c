00141cbf51d4b166339014634c74fe19ef22ce69
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-21 12:06:18 -0700

    not_found

--- error_prompt.c
+++ error_prompt.c
@@ -32,3 +32,36 @@ void invalid_number(list_t *env, char *str, int c_n)
 	write(STDOUT_FILENO, count, str);
 	write(STDOUT_FILENO, "\n", 1);
 }
+
+/**
+ * not_found - Print error ("sh: 1: lss: not found")
+ * @str: String arguement typed
+ * @c_n: command typed by nth user
+ * @env: Enviromental variables linked list
+ */
+
+void not_found(char *str, int c_n, list_t *env);
+{
+	char *shell, *num;
+	int count = 0;
+
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	num = int_to_string(c_n);
+	count = 0;
+	while (num[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, count, num);
+	free(num);
+	write(STDOUT_FILENO, ": ", 2);
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "not found\n", 10);
+}

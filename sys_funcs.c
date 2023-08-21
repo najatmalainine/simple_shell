794b5bdb4422aa633920f41fdbde4c650be83422
Najat <craazylolla@gmail.com>
2023-08-21 15:42:36 -0700

    first commit

---
+++ sys_funcs.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * _which - finds command by appending it to a matching PATH directory
+ * @s: the first command user typed
+ * @envi: environmental variable
+ * Return: a copy of the command
+ */
+char *_which(char *s, list_t *envi)
+{
+	char *path, *buf = NULL, **tokens;
+	int i = 0;
+
+	path = get_env("PATH", envi);
+	tokens = c_strtok(path, ":");
+	free(path);
+
+	i = 0;
+	while (tokens[i] != NULL)
+	{
+		if (tokens[i][0] == '\0')
+			buf = getcwd(buf, 0);
+		else
+			buf = _strdup(tokens[i]);
+		buf = _strcat(buf, "/");
+		buf = _strcat(buf, s);
+		if (access(buf, F_OK) == 0)
+		{
+			free_db(tokens);
+			return (buf);
+		}
+		free(buf);
+		i++;
+	}
+	free_db(tokens);
+	return (s);
+}

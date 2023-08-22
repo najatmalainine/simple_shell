149606f46850b3ffee25223764d3a52eb389dd9e
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-22 15:03:02 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * main - The function creates simple shell
+ * @agc: Arguement count
+ * @agv: Arguement vector
+ * @enm: Envorimental variables
+ * Return: For success 0
+ */
+int main(int agc, char **agv, char **enm)
+{
+	(void)agc;
+	(void)agv;
+
+	prompt(enm);
+	return (0);
+}

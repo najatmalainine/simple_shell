7b541a8f2984db131fd64b5e567a8e258f972923
Najat <craazylolla@gmail.com>
2023-08-22 16:30:31 -0700

    _exit

--- exit.c
+++ exit.c
@@ -20,3 +20,30 @@ int _atoi(char *s)
 	}
 	return (num);
 }
+/**
+ * ___exit - it frees the user 's input and then exits
+ * @s: user's command
+ * @envi: environmental variable
+ * @n: nth user command line
+ * @cmd: the command to free
+ * Return: 0 if it successeds 2 if it fails
+ */
+int ___exit(char **s, list_t *envi, int n, char **cmd)
+{
+	int value = 0;
+
+	if (s[1] != NULL)
+		value = _atoi(s[1]);
+
+	if (value == -1)
+	{
+		invalid_number(s[1], n, envi);
+		free_db(s);
+		return (2);
+	}
+	free_db(s);
+	free_linked_list(envi);
+	if (cmd != NULL)
+		free_db(cmd);
+	exit(value);
+}

91871ec29299def380c5412a526231f1a59073bd
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-22 14:42:28 -0700

    error

--- error_prompt.c
+++ error_prompt.c
@@ -2,99 +2,99 @@
 
 /**
  * invalid_number - prints error ("sh: 3: exit: number invalid")
- * @str: an arguement typed after the command exit
- * @c_n: nth times user typed command
- * @env: enviromental variables linked list
+ * @s: an arguement typed after the command exit
+ * @c_no: nth times user typed command
+ * @enm: enviromental variables linked list
  */
 
-void invalid_number(list_t *env, char *str, int c_n)
+void invalid_number(list_t *enm, char *s, int c_no)
 {
-	char *shell = NULL, *num = NULL;
+	char *shell = NULL, *n = NULL;
 	int count = 0;
 
-	shell = _getenv("_", env);
+	shell = _getenm("_", enm);
 	while (shell[count] != '\0')
 		count++;
 	write(STDOUT_FILENO, count, shell);
 	free(shell);
 	write(STDOUT_FILENO, ": ", 2);
-	num = int_to_string(c_n);
+	n = int_to_string(c_no);
 	count = 0;
-	while (num[count] != '\0')
+	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, num);
-	free(num);
+	write(STDOUT_FILENO, count, n);
+	free(n);
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "exit: invalid number: ", 22);
 	count = 0;
-	while (str[count] != '\0')
+	while (s[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, str);
+	write(STDOUT_FILENO, count, s);
 	write(STDOUT_FILENO, "\n", 1);
 }
 
 /**
  * not_found - Print error ("sh: 1: lss: not found")
- * @str: String arguement typed
- * @c_n: command typed by nth user
- * @env: Enviromental variables linked list
+ * @s: String arguement typed
+ * @c_no: command typed by nth user
+ * @enm: Enviromental variables linked list
  */
 
-void not_found(char *str, int c_n, list_t *env);
+void not_found(char *s, int c_no, list_t *enm);
 {
-	char *shell, *num;
+	char *shell, *n;
 	int count = 0;
 
-	shell = _getenv("_", env);
+	shell = _getenm("_", enm);
 	while (shell[count] != '\0')
 		count++;
 	write(STDOUT_FILENO, shell, count);
 	free(shell);
 	write(STDOUT_FILENO, ": ", 2);
-	num = int_to_string(c_n);
+	n = int_to_string(c_no);
 	count = 0;
-	while (num[count] != '\0')
+	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, num);
-	free(num);
+	write(STDOUT_FILENO, count, n);
+	free(n);
 	write(STDOUT_FILENO, ": ", 2);
 	count = 0;
-	while (str[count] != '\0')
+	while (s[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, s, count);
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "not found\n", 10);
 }
 
 /**
  * cmd_invalid - Prints error ("sh: 2: cd: cant cd to xyz")
- * @env: Enviromental variables linked list
- * @c_n: Command typed by user
- * @str: Arguement typed after cmd
+ * @enm: Enviromental variables linked list
+ * @c_no: Command typed by user
+ * @s: Arguement typed after cmd
  */
 
-void cmd_invalid(list_t *env, int c_n, char *str)
+void cmd_invalid(list_t *enm, int c_no, char *s)
 {
-	char *shell, *num;
+	char *shell, *n;
 	int count = 0;
 
-	shell = _getenv("_", env);
+	shell = _getenm("_", enm);
 	while (shell[count] != '\0')
 		count++;
 	write(STDOUT_FILENO, shell, count);
 	free(shell);
 	write(STDOUT_FILENO, ": ", 2);
-	num = int_to_string(c_n);
+	n = int_to_string(c_no);
 	count = 0;
-	while (num[count] != '\0')
+	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, num);
-	free(num);
+	write(STDOUT_FILENO, count, n);
+	free(n);
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "cd: can't cd to " 16);
 	count = 0;
-	while (str[count] != '\0')
+	while (s[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, s, count);
 	write(STDOUT_FILENO, "\n", 1);
 }

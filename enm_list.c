ed2214054f38ffce1efb39e85b88314b9842bc95
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-08-22 13:40:34 -0700

    enm

--- enm_list.c
+++ enm_list.c
@@ -19,3 +19,18 @@ list_t *enm_linked_list(char **enm)
 	}
 	return (hd);
 }
+
+/**
+ * _enm - To prints enviromental varibles
+ * @s: String command
+ * @enm: Enviromental variables
+ * Return: For success 0
+ */
+
+int _enm(char **s, list_t *enm)
+{
+	free_double_ptr(s);
+	print_list(enm);
+	return (0);
+}
+

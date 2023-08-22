3b5279346ccf2b1298574d9aa7577d75c5e7d787
Najat <craazylolla@gmail.com>
2023-08-22 06:50:57 -0700

    changes

--- prompt_cmd.c
+++ prompt_cmd.c
@@ -1,113 +1,17 @@
-#include "main.shell"
+#include "shell.h"
 
 /**
- * prints_prompt - Repeatedly prints user typed command
- * @ev: Typed in Enviromental variables
- * Return: 0 for success
+ * ctrl_d - if ctrl-d was typed exit program
+ * @c: the characters that _getline reads
+ * @cmd: the user's command
+ * @envi: environment variable
  */
-int prints_prompt(char **ev)
+void ctrl_d(int c, char *cmd, list_t *envi)
 {
-	list_t *env;
-	char *command, *n_command, **token;
-	int command_line_no = 0, exit_stat = 0;
-	size_t j = 0, m = 0;
-
-	env = env_linked_list(ev);
-	do {
-		command_line_no++;
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILE, "$ ", 2);
-		else
-			non_interavctive(env);
-		signal(SIGNAL, ctrl_c);
-		command = NULL, j = 0;
-		j = get_line(&command);
-		ctrl_D(j, command, env);
-		n_command = command;
-		command = ignore_space(command);
-		m = 0;
-		while (command[m] != '\n')
-			m++;
-		command[m] '\0';
-		if (command[0] == '\0')
-		{
-			free(n_command);
-			continue;
-		}
-		token = NULL, token = _str_tok(command, " ");
-		if (n_command != NULL)
-			free(n_command);
-		exit_stat = built_in(token, command_line_no, env, NULL);
-		if (exit_stat)
-			continue;
-		exit_stat = _execve(env, token, command_line_no);
-	} while (1);
-	return (exit_stat);
-}
-
-/**
- * ctrl_c - Prints prompts by ignoring ctrl-C command
- * @m: Inputs signal
- */
-void ctrl_c(int m)
-{
-	(void)m;
-
-	write(STDOUT_FILENO, "\n$ ", 3);
-}
-
-/**
- * handles_builtin - builtin functions are taken care of (exit, env, cd)
- * @command: As user typed, brings command to free
- * @token: typed in  command
- * @env: typed in Enviromental variables
- * @num: Write error message after taking in command
- * Return: 1 for success, else 0
- */
-
-int handles_builtin(char **token, int num, char **command, list_t *env)
-{
-	int j = 0;
-
-	if (_strcmp(token[0], "exit") == 0)
+	if (c == 0)
 	{
-		j = _exit(token, command, num, env);
-	}
-	else if (_strcmp(token[0], "unsetenv") == 0)
-	{
-		_unsetenv(&env, token);
-		j = 1;
-	}
-	else if (_strcmp(token[0], "setenv") == 0)
-	{
-		_setenv(&env, token);
-		j = 1;
-	}
-	else if (_strcmp(token[0], "cd") == 0)
-	{
-		j = _cd(token, num, env);
-	}
-	else if (_strcmp(token[0], "env") == 0)
-	{
-		_env(env, token);
-		j = 1;
-	}
-	return (1);
-}
-
-/**
- * ctrl_D - Exit program if user typed ctrl-d
- * @env: Linked list enviromental variables
- * @j: get line character read in value
- * @command: Typed in command
- */
-
-void ctrl_D(int j, list_t *env, char *command)
-{
-	if (j == 0)
-	{
-		free(command);
-		free_linked_list(env);
+		free(cmd);
+		free_linked_list(envi);
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "\n", 1);
 		exit(0);
@@ -115,13 +19,110 @@ void ctrl_D(int j, list_t *env, char *command)
 }
 
 /**
- * takein_space - while user typed command, return str without space in front
- * @str: User tyed input string
- * Return: String, for success
+ * _ctrl_c - Prints prompts by ignoring ctrl-C command
+ * @num: Inputs signal
  */
-char takein_space(char *str)
+void _ctrl_c(int num)
 {
-	while (*str == ' ')
-		str++;
-	return (str);
+	(void)num;
+	write(STDOUT_FILENO, "\n$ ", 3);
+}
+
+
+/**
+ * takein_space - it returns the string without
+ * @s: string
+ * Return: string
+ */
+char *takein_space(char *s)
+{
+	while (*s == ' ')
+		s++;
+	return (s);
+}
+
+/**
+ * _builtin - builtin functions
+ * @tkn: the typed in command
+ * @envi: the environmental variable
+ * @n: the nth user command typed for errors
+ * @cmd: the command to free
+ * Return: 1 if acted on builtin, 0 if not
+ */
+int _builtin(char **tkn, list_t *envi, int n, char **cmd)
+{
+	int i = 0;
+
+	if (_strcmp(tkn[0], "exit") == 0)
+	{
+		i = ___exit(tkn, envi, n, cmd);
+	}
+	else if (_strcmp(tkn[0], "env") == 0)
+	{
+		_env(tkn, envi);
+		i = 1;
+	}
+	else if (_strcmp(tkn[0], "cd") == 0)
+	{
+		i = _cd(tkn, envi, n);
+	}
+	else if (_strcmp(tkn[0], "setenv") == 0)
+	{
+		_setenv(&envi, tkn);
+		i = 1;
+	}
+	else if (_strcmp(tkn[0], "unsetenv") == 0)
+	{
+		_unsetenv(&envi, tkn);
+		i = 1;
+	}
+	return (i);
+}
+
+
+/**
+ * prints_prompt - Repeatedly prints user typed command
+ * @ev: envrionmental variables
+ * Return: 0 on success
+ */
+int prints_prompt(char **ev)
+{
+	list_t *envi;
+	size_t j = 0, num = 0;
+	int cmd_no = 0, ex_status = 0;
+	char *cmd, *n_cmd, **tkn;
+
+	envi = env_linked_list(ev);
+	do {
+		cmd_no++;
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+		else
+			non_interactive(envi);
+		signal(SIGINT, _ctrl_c);
+		cmd = NULL;
+		j = 0;
+		j = _getline(&cmd);
+		ctrl_d(j, cmd, envi);
+		n_cmd = cmd;
+		cmd = takein_space(cmd);
+		num = 0;
+		while (cmd[num] != '\n')
+			num++;
+		cmd[num] = '\0';
+		if (cmd[0] == '\0')
+		{
+			free(n_cmd);
+			continue;
+		}
+		tkn = NULL;
+		tkn = _strtok(cmd, " ");
+		if (n_cmd != NULL)
+			free(n_cmd);
+		ex_status = built_in(tkn, envi, cmd_no, NULL);
+		if (ex_status)
+			continue;
+		ex_status = _execve(tkn, envi, cmd_no);
+	} while (1);
+	return (ex_status);
 }

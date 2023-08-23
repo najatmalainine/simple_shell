95c91fdfada0588542bf2d8d6c55b805e4cf162e
Najat <craazylolla@gmail.com>
2023-08-23 06:49:16 -0700

    fixes

--- error_prompt.c
+++ error_prompt.c
@@ -1,40 +1,40 @@
 #include "shell.h"
 
 /**
- * invalid_number - prints error ("sh: 3: exit: number invalid")
+ * invalid_number - prints error
  * @s: an arguement typed after the command exit
  * @c_no: nth times user typed command
  * @enm: enviromental variables linked list
  */
 
-void invalid_number(list_t *enm, char *s, int c_no)
+void invalid_number(char *s, int c_no, list_t *enm)
 {
 	char *shell = NULL, *n = NULL;
 	int count = 0;
 
-	shell = _getenm("_", enm);
+	shell = _getenv("_", enm);
 	while (shell[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, shell);
+	write(STDOUT_FILENO, shell, count);
 	free(shell);
 	write(STDOUT_FILENO, ": ", 2);
 	n = int_to_string(c_no);
 	count = 0;
 	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, n);
+	write(STDOUT_FILENO, n, count);
 	free(n);
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "exit: invalid number: ", 22);
 	count = 0;
 	while (s[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, s);
+	write(STDOUT_FILENO, s, count);
 	write(STDOUT_FILENO, "\n", 1);
 }
 
 /**
- * not_found - Print error ("sh: 1: lss: not found")
+ * not_found - Print error
  * @s: String arguement typed
  * @c_no: command typed by nth user
  * @enm: Enviromental variables linked list
@@ -45,7 +45,7 @@ void not_found(char *s, int c_no, list_t *enm)
 	char *shell, *n;
 	int count = 0;
 
-	shell = _getenm("_", enm);
+	shell = _getenv("_", enm);
 	while (shell[count] != '\0')
 		count++;
 	write(STDOUT_FILENO, shell, count);
@@ -55,7 +55,7 @@ void not_found(char *s, int c_no, list_t *enm)
 	count = 0;
 	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, n);
+	write(STDOUT_FILENO, n, count);
 	free(n);
 	write(STDOUT_FILENO, ": ", 2);
 	count = 0;
@@ -67,18 +67,18 @@ void not_found(char *s, int c_no, list_t *enm)
 }
 
 /**
- * cmd_invalid - Prints error ("sh: 2: cd: cant cd to xyz")
+ * cmd_invalid - Prints error
  * @enm: Enviromental variables linked list
  * @c_no: Command typed by user
  * @s: Arguement typed after cmd
  */
 
-void cmd_invalid(list_t *enm, int c_no, char *s)
+void cmd_invalid(char *s, int c_no, list_t *enm)
 {
 	char *shell, *n;
 	int count = 0;
 
-	shell = _getenm("_", enm);
+	shell = _getenv("_", enm);
 	while (shell[count] != '\0')
 		count++;
 	write(STDOUT_FILENO, shell, count);
@@ -88,10 +88,10 @@ void cmd_invalid(list_t *enm, int c_no, char *s)
 	count = 0;
 	while (n[count] != '\0')
 		count++;
-	write(STDOUT_FILENO, count, n);
+	write(STDOUT_FILENO, n, count);
 	free(n);
 	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, "cd: can't cd to " 16);
+	write(STDOUT_FILENO, "cd: can't cd to ", 16);
 	count = 0;
 	while (s[count] != '\0')
 		count++;
--- shell.h
+++ shell.h
@@ -1,61 +1,62 @@
-#ifndef  SHELL
+#ifndef SHELL
 #define SHELL
 
-#include <signal.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+int prints_prompt(char **env);
+void *_realloc(void *ptr, unsigned int size_old, unsigned int size_new);
+size_t _getline(char **str);
+int t_strlen(char *str, int pos, char delm);
+char *takein_space(char *str);
+char **_strtok(char *str, char *delm);
+char **c_strtok(char *str, char *delm);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _cd(char **cmd, list_t *envi, int n);
+int _builtin(char **token, list_t *env, int num, char **command);
+void non_interactive(list_t *env);
+char *_which(char *str, list_t *envi);
+int ___exit(char **s, list_t *envi, int n, char **cmd);
+int _execve(char **c, list_t *envi, int n);
+void free_db(char **str);
+void free_linked_list(list_t *list);
+int _enm(char **str, list_t *env);
+char *_getenv(char *str, list_t *env);
+list_t *enm_linked_list(char **env);
+list_t *add_end_node(list_t **head, char *str);
+size_t print_list(list_t *h);
+int node_index_rm(list_t **head, int index);
+int _unsetenv(list_t **env, char **str);
+int _setenv(list_t **env, char **str);
+int f_envi(list_t *env, char *str);
+void not_found(char *str, int num, list_t *env);
+void cmd_invalid(char *str, int c_n, list_t *env);
+void invalid_number(char *str, int c_n, list_t *env);
+char *int_to_string(int num);
+
 
 /**
- * struct list - Structure linked list
- * @vr: String of enviromental variables
- * @nxt: A pointer to the next node available
+ * struct list - linked list for environmental variables
+ * @var: holds environmental variable string
+ * @next: points to next node
  */
 typedef struct list
 {
-	char *vr;
-	struct list *nxt;
+	char *var;
+	struct list *next;
 
 } list_t;
 
-/* All the functions prototypes for the simple_shell project */
-void invalid_number(list_t *enm, char *s, int c_no);
-void not_found(char *s, int c_no, list_t *enm);
-void cmd_invalid(list_t *enm, int c_no, char *s);
-void ctrl_d(int c, char *cmd, list_t *enm);
-void _ctrl_c(int num);
-void *_realloc(void *ptr, unsigned int size_old, unsigned int size_new);
-void f_exit(char **str, list_t *envi);
-void cd_home(list_t *envi, char *cur);
-void non_interactive(list_t *enm);
-void free_db(char **s);
-int _builtin(char **tkn, list_t *envi, int n, char **cmd);
-int prints_prompt(char **ev);
-int cd_exec(list_t *envi, char *cur, char *dir, char *str, int n);
-int _execve(char **c, list_t *envi, int n);
-int f_setenv(list_t **envi, char *name, char *dir);
-int _cd(char **cmd, list_t *envi, int n);
-int _atoi(char *s);
-int _enm(char **s, list_t *enm);
-int ___exit(char **s, list_t *envi, int n, char **cmd);
-int _strcmp(char *s1, char *s2);
-char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
-char *takein_space(char *s);
-char *ignore_c(char *s);
-char *_strcat(char *dest, char *src);
-char *_which(char *s, list_t *envi);
-char *f_strcat(char *dest, char *src);
-size_t _getline(char **str);
-list_t *enm_linked_list(char **enm);
-
-
 #endif
-
